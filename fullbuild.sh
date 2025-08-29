fff67cf24275fd74aceae7f6c4c0c3d79e238eff
iamjakeirl <jakemercadante1@gmail.com>
2025-08-29 17:12:17 -0600

    Update .gitignore and remove tracked files that should be ignored

--- fullbuild.sh
+++
@@ -1,4 +0,0 @@
-cd ~/projects/cursor/jorkScripts/jorkscripts
-./gradlew clean build --refresh-dependencies --rerun-tasks --no-build-cache
-
-./gradlew clean buildAllVariants --refresh-dependencies --rerun-tasks --no-build-cache
